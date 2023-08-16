bbf33533e1cd06c619ac71d589c7ce93604752c2
Aylu <128099082+ayluernandes@users.noreply.github.com>
2023-08-16 19:42:33 -0300

    Add files via upload

--- JS Curso/Sistema de Cadastro/codigo.js
+++ JS Curso/Sistema de Cadastro/codigo.js
@@ -30,7 +30,7 @@ function menu() {
 
 function cadastrar_cliente() {
     let x = "n";
-    relatorio = "*- Relatório de Clientes -* <br>"
+    relatorio = " Relatório de Clientes <br>"
 
     while (x.toUpperCase() != "S") {
         id = parseInt(prompt("Digite o ID do cliente"));
@@ -38,12 +38,61 @@ function cadastrar_cliente() {
         endereco = prompt("Digite o endereço do cliente");
 
         relatorio = relatorio + "<br>" +
-            "ID: " + id + "<br>" +
             "Nome: " + nome + "<br>" +
-            "Endereço: " + endereco +
-            "<br>"
+            "Endereço: " + endereco + "<br>" +
+            "ID: " + id
 
-        confirme = prompt("Finalizar Cadastro - S para Sim - N para Não");
+        let confirme = prompt("Finalizar Cadastro? - S para Sim - N para Não");
+
+        if (confirme.toUpperCase() == "S") {
+            x = "S";
+        };
+        document.getElementById("relatorio").innerHTML = "<p>" + relatorio + "</p>"
+    }
+}
+
+function cadastrar_colaborador() {
+    let x = "n";
+    relatorio = " Relatório de Colaboradores <br>"
+
+    while (x.toUpperCase() != "S") {
+        id = parseInt(prompt("Digite o ID do colaborador"));
+        nome = prompt("Digite o nome do colaborador");
+        funcao = prompt("Digite a função do colaborador");
+        empresa = prompt("Em que empresa exerce suas funções?");
+
+        relatorio = relatorio + "<br>" +
+            "Nome: " + nome + "<br>" +
+            "Função: " + funcao + "<br>" +
+            "Empresa: " + empresa + "<br>" +
+            "ID: " + id
+
+        confirme = prompt("Finalizar Cadastro? - S para Sim - N para Não");
+
+        if (confirme.toUpperCase() == "S") {
+            x = "S";
+        };
+        document.getElementById("relatorio").innerHTML = "<p>" + relatorio + "</p>"
+    }
+}
+
+function cadastrar_produto() {
+    let x = "n";
+    relatorio = " Relatório de Produto <br>"
+
+    while (x.toUpperCase() != "S") {
+        produto = prompt("Digite o nome do produto");
+        codigo = parseInt(prompt("Digite o código do produto"));
+        validade = prompt("Digite a data de validade do produto");
+        marca = prompt("Digite a marca ou fabricante do produto");
+
+        relatorio = relatorio + "<br>" +
+            "Produto: " + produto + "<br>" +
+            "Código do produto: " + codigo + "<br>" +
+            "Validade: " + validade + "<br>" +
+            "Marca/Fabricante: " + marca + "<br>"
+
+        confirme = prompt("Finalizar cadastro? - S para Sim - N para Não");
 
         if (confirme.toUpperCase() == "S") {
             x = "S";
